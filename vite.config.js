f1ab387947ae50c456584f6a0d0f53cb5e2321aa
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-11 16:21:32 +0530

    build: remove @material-ui from vite external config

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
       external: [
         /^@babel.*/,
         /^@date-io\/.*/,
-        /^@material-ui\/.*/,
+
         /^@openimis.*/,
         'classnames',
         'clsx',
