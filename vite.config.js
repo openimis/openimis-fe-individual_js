2bb1ed169ff1fa854e9462d9e1a5004dd7cf27cf
olewandowski1 <olewandowski@soldevelo.com>
2026-02-06 15:27:42 +0100

    Externalize MUI/Emotion in build

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,13 @@ export default defineConfig({
         'prop-types',
         /^react.*/,
         /^redux.*/,
+        '@mui/material',
+        '@mui/icons-material',
+        '@mui/x-date-pickers',
+        '@emotion/react',
+        '@emotion/styled',
+        '@emotion/cache',
+        '@mui/styled-engine',
       ],
       output: {
         globals: {
